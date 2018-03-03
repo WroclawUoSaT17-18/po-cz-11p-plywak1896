a9ca02b4577939bb13049a3a7a7d805df3c62bb5
plywak1896 <plywak1896@gmail.com>
2018-03-03 22:49:42 +0100

    Initial commit

---
+++ PierwszaAplikacja/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PierwszaAplikacja
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("Witam w programie liczącym średnią.");
+            Console.WriteLine("Proszę podać pierwszą ocenę:");
+            float d = float.Parse(Console.ReadLine());
+            Console.WriteLine("Proszę podać drugą ocenę:");
+            float e = float.Parse(Console.ReadLine());
+            Console.WriteLine("Proszę podać trzecią ocenę:");
+            float f = float.Parse(Console.ReadLine());
+            student student1 = new student(d,e,f);
+            Console.WriteLine(student1.srednia());
+            Console.ReadKey(); 
+        }
+    }
+}
+
+class student
+{
+    private float a ;
+    private float b ;
+    private float c ;
+
+    public student(float a1,float b2,float c3)
+    {
+     a = a1;
+     b = b2;
+     c = c3;
+}
+    public float srednia()
+    {
+    Console.WriteLine("Średnia ma wartość:");
+        return ((a + b + c) / 3);
+        
+    }
+}
