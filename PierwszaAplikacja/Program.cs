4df23a85a6add9e0917deaf5d7e66839666b2b6d
plywak1896 <plywak1896@gmail.com>
2018-03-07 23:34:01 +0100

    Jakub P
    Dodanie większej ilości studentów.

--- PierwszaAplikacja/Program.cs
+++ PierwszaAplikacja/Program.cs
@@ -12,14 +12,24 @@ namespace PierwszaAplikacja
         {
             Console.BackgroundColor = ConsoleColor.DarkCyan;
             Console.WriteLine("Witam w programie liczącym średnią.");
-            Console.WriteLine("Proszę podać pierwszą ocenę:");
-            float d = float.Parse(Console.ReadLine());
-            Console.WriteLine("Proszę podać drugą ocenę:");
-            float e = float.Parse(Console.ReadLine());
-            Console.WriteLine("Proszę podać trzecią ocenę:");
-            float f = float.Parse(Console.ReadLine());
-            student student1 = new student(d,e,f);
-            Console.WriteLine(student1.Srednia());
+            Console.WriteLine("Proszę podać ilość studentów:");
+            int ilosc = int.Parse(Console.ReadLine());
+
+            for (int g = 0 ; g < ilosc; g++) 
+            {
+                Console.WriteLine("Proszę podać numer indeksu:");
+                int h = int.Parse(Console.ReadLine());
+                Console.WriteLine("Proszę podać pierwszą ocenę (AiSUK):");
+                double d = double.Parse(Console.ReadLine());
+                Console.WriteLine("Proszę podać drugą ocenę (Podstawy Automatyki):");
+                double e = double.Parse(Console.ReadLine());
+                Console.WriteLine("Proszę podać trzecią ocenę (Metrologia Elektryczna):");
+                double f = double.Parse(Console.ReadLine());
+                student student1 = new student(d, e, f, h);
+                Console.WriteLine(student1.Srednia());
+                
+            }
+            
             Console.ReadKey(); 
         }
     }
@@ -27,21 +37,28 @@ namespace PierwszaAplikacja
 
 class student
 {
-    private float a ;
-    private float b ;
-    private float c ;
+    private double a ;
+    private double b ;
+    private double c ;
+    private int d;
 
-    public student(float a1,float b2,float c3)
+    public student(double a1, double b2, double c3, int d4)
     {
      a = a1;
      b = b2;
      c = c3;
+     d = d4;
 }
-    public float Srednia()
+    public double Srednia()
     {
-    Console.WriteLine("Średnia ma wartość:");
-        return ((a + b + c) / 3);
+    Console.WriteLine("Średnia studenta nr. " + d + " ma wartość:");
+        int f = 3;
+        double g = ((a + b + c) / f );
+
+        return Math.Round(g, 2);
         
+
+
     }
 }
 
